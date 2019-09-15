d2e120fa142828f6ccfed0b08cbe49e6677e1c36
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-09-15 11:47:37 -0600

    yeah

--- src/Orders.js
+++ src/Orders.js
@@ -1,13 +1,13 @@
 import domUpdates from "./domUpdates";
 
 class Orders {
-    constructor(userID, serviceDate, food, cost) {
-      this.userID = userID;
-      this.serviceDate = serviceDate;
-      this.food = food;
-      this.cost = cost;
-    }
+  constructor(userID, serviceDate, food, cost) {
+    this.userID = userID;
+    this.serviceDate = serviceDate;
+    this.food = food;
+    this.cost = cost;
   }
+}
 
 
 export default Orders;
